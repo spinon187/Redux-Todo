42029579020b04f0de1c83989ab70c4aff3924b1
spinon187 <spinon187@gmail.com>
2019-01-28 16:51:26 -0600

    redux todo initial commit

---
+++ todo/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { createStore } from 'redux';
+import {Provider} from 'react-redux';
+
+const store = createStore(todoList);
+
+ReactDOM.render(
+    <Provider store={store}>
+      <todoList />
+    </Provider>,
+    document.getElementById('root')
+  );
